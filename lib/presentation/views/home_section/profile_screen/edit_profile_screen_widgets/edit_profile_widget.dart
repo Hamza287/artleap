817725a377e5becd8f5bb8bddaba9cf1f6e822a5
MimarStudio <mimar@Mimars-Mac-mini.local>
2025-07-04 15:36:13 +0500

    main changes done

--- lib/presentation/views/home_section/profile_screen/edit_profile_screen_widgets/edit_profile_widget.dart
+++ lib/presentation/views/home_section/profile_screen/edit_profile_screen_widgets/edit_profile_widget.dart
@@ -14,127 +14,128 @@ class EditProfileWidget extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Padding(
-      padding: const EdgeInsets.only(left: 20),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          40.spaceY,
-
-          // 30.spaceY,
-          GestureDetector(
-            onTap: () {
-              Navigation.pop();
-            },
-            child: Icon(
-              Icons.arrow_back_rounded,
-              color: AppColors.white,
-            ),
-          ),
-          40.spaceY,
-          Row(
-            children: [
-              Container(
-                height: 75,
-                width: 75,
-                decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    image: DecorationImage(
-                        image: params!.profileImage == ''
-                            ? AssetImage(
-                                AppAssets.profilepic,
-                              )
-                            : NetworkImage(
-                                params!.profileImage ?? AppAssets.profilepic,
-                              ),
-                        fit: BoxFit.cover)),
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.only(left: 20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            40.spaceY,
+      
+            // 30.spaceY,
+            GestureDetector(
+              onTap: () {
+                Navigation.pop();
+              },
+              child: Icon(
+                Icons.arrow_back_rounded,
+                color: AppColors.white,
               ),
-              20.spaceX,
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    params!.userName ?? "user name",
-                    style: AppTextstyle.interMedium(
-                        color: AppColors.white, fontSize: 14),
-                  ),
-                  10.spaceY,
-                  Text(
-                    params!.userEmail ?? "username@gmail.com",
-                    style: AppTextstyle.interMedium(
-                        color: AppColors.white, fontSize: 12),
-                  ),
-                ],
-              )
-            ],
-          ),
-          10.spaceY,
-          50.spaceY,
-          SeparatorWidget(
-            title: "General",
-          ),
-          20.spaceY,
-
-          GestureDetector(
-            onTap: () {
-              Navigator.pushNamed(
-                context,
-                PrivacyPolicyScreen.routeName,
-              );
-            },
-            child: IconWithTextTile(
-              imageIcon: AppAssets.privacyicon,
-              title: "Privacy Policy",
             ),
-          ),
-          50.spaceY,
-          GestureDetector(
-            onTap: () {
-              Navigator.pushNamed(
-                context,
-                HelpScreen.routeName,
-              );
-            },
-            child: IconWithTextTile(
-              imageIcon: AppAssets.questionmark,
-              title: "Help",
+            40.spaceY,
+            Row(
+              children: [
+                Container(
+                  height: 75,
+                  width: 75,
+                  decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      image: DecorationImage(
+                          image: params!.profileImage == ''
+                              ? AssetImage(
+                                  AppAssets.profilepic,
+                                )
+                              : NetworkImage(
+                                  params!.profileImage ?? AppAssets.profilepic,
+                                ),
+                          fit: BoxFit.cover)),
+                ),
+                20.spaceX,
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      params!.userName ?? "user name",
+                      style: AppTextstyle.interMedium(
+                          color: AppColors.white, fontSize: 14),
+                    ),
+                    10.spaceY,
+                    Text(
+                      params!.userEmail ?? "username@gmail.com",
+                      style: AppTextstyle.interMedium(
+                          color: AppColors.white, fontSize: 12),
+                    ),
+                  ],
+                )
+              ],
             ),
-          ),
-          50.spaceY,
-          SeparatorWidget(
-            title: "About",
-          ),
-          20.spaceY,
-          InkWell(
-            onTap: () {
-              AppLocal.ins.clearUSerData(Hivekey.userId);
-              Navigation.pushNamedAndRemoveUntil(LoginScreen.routeName);
-            },
-            child: IconWithTextTile(
-              imageIcon: AppAssets.logouticon,
-              title: "Logout",
-              titleColor: AppColors.redColor,
+            50.spaceY,
+            SeparatorWidget(
+              title: "General",
             ),
-          ),
-          20.spaceY,
-          InkWell(
-            onTap: () {
-              showDialog(
-                context: context,
-                builder: (context) => const DeleteAccountDialog(),
-              );
-            },
-            child: IconWithTextTile(
-              imageIcon: AppAssets.deleteicon,
-              title: "Delete Account",
-              titleColor: AppColors.redColor,
+            20.spaceY,
+      
+            GestureDetector(
+              onTap: () {
+                Navigator.pushNamed(
+                  context,
+                  PrivacyPolicyScreen.routeName,
+                );
+              },
+              child: IconWithTextTile(
+                imageIcon: AppAssets.privacyicon,
+                title: "Privacy Policy",
+              ),
             ),
-          ),
-          80.spaceY,
-
-          // NativeAdWidget()
-        ],
+            20.spaceY,
+            GestureDetector(
+              onTap: () {
+                Navigator.pushNamed(
+                  context,
+                  HelpScreen.routeName,
+                );
+              },
+              child: IconWithTextTile(
+                imageIcon: AppAssets.questionmark,
+                title: "Help",
+              ),
+            ),
+            50.spaceY,
+            SeparatorWidget(
+              title: "About",
+            ),
+            20.spaceY,
+            InkWell(
+              onTap: () {
+                AppLocal.ins.clearUSerData(Hivekey.userId);
+                Navigation.pushNamedAndRemoveUntil(LoginScreen.routeName);
+              },
+              child: IconWithTextTile(
+                imageIcon: AppAssets.logouticon,
+                title: "Logout",
+                titleColor: AppColors.redColor,
+              ),
+            ),
+            20.spaceY,
+            InkWell(
+              onTap: () {
+                showDialog(
+                  context: context,
+                  builder: (context) => const DeleteAccountDialog(),
+                );
+              },
+              child: IconWithTextTile(
+                imageIcon: AppAssets.deleteicon,
+                title: "Delete Account",
+                titleColor: AppColors.redColor,
+              ),
+            ),
+            80.spaceY,
+      
+            // NativeAdWidget()
+          ],
+        ),
       ),
     );
   }
