5e1bca3c6ef07ea6c5cc70d394858cccd71ad8e2
MimarStudio <mimar@Mimars-Mac-mini.local>
2025-10-07 17:22:37 +0500

    notification fix

--- lib/presentation/views/subscriptions/choose_plan_screen.dart
+++ lib/presentation/views/subscriptions/choose_plan_screen.dart
@@ -2,10 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:Artleap.ai/shared/constants/app_colors.dart';
 import 'package:Artleap.ai/shared/constants/app_textstyle.dart';
-import '../../../domain/subscriptions/subscription_model.dart';
 import 'choose_plan_widgets/plan_selection_content.dart';
-
-final selectedPlanProvider = StateProvider<SubscriptionPlanModel?>((ref) => null);
 
 class ChoosePlanScreen extends ConsumerWidget {
   const ChoosePlanScreen({super.key});
@@ -13,40 +10,33 @@ class ChoosePlanScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final size = MediaQuery.of(context).size;
+
     return Scaffold(
       extendBodyBehindAppBar: true,
+      backgroundColor: Colors.black,
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         elevation: 0,
-        title: Text(
-          'Choose Your Plan',
-          style: AppTextstyle.interBold(
-            fontSize: 20,
-            color: AppColors.white,
-          ),
-        ),
-        centerTitle: true,
         iconTheme: const IconThemeData(color: AppColors.white),
-        actions: [
-          // IconButton(
-          //   icon: const Icon(Icons.refresh),
-          //   onPressed: () {
-          //     ref.read(selectedPlanProvider.notifier).state = null;
-          //     ref.invalidate(subscriptionPlansProvider);
-          //   },
-          // ),
-        ],
+        title: Text(
+          ' ',
+          style: AppTextstyle.interBold(fontSize: 20, color: AppColors.white),
+        ),
       ),
       body: Stack(
         children: [
           Positioned.fill(
             child: Image.asset(
-              'assets/images/planbg.png',
+              'assets/images/subscription_bg.png',
               fit: BoxFit.cover,
             ),
           ),
-          const SafeArea(
-            child: PlanSelectionContent(),
+          SafeArea(
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: size.width * 0.04),
+              child: const PlanSelectionContent(),
+            ),
           ),
         ],
       ),
